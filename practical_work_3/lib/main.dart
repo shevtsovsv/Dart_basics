0cf6c74e67e9d60ed92aaab92a06b4b1f37ddf62
Serei Shevtsov <Shevtsovsv@yahoo.com>
2022-12-07 18:24:13 +0300

    revision 1

--- practical_work_3/lib/main.dart
+++ practical_work_3/lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           backgroundColor: Colors.amber[300],
           appBar: AppBar(
-            title: Text('Это Flutter'),
+            title: const Text('Это Flutter'),
           ),
           body: Center(
             child: Column(
@@ -41,25 +41,9 @@ class MyApp extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
                   Text('${Uuid().v4()}'),
-
-
-                  // HintLabel('label 1'),
-                  //  Shimmer.fromColors(
-                  //     baseColor: Colors.red,
-                  //     highlightColor: Colors.yellow,
-                  //     child: Text(
-                  //       'Shimmer',
-                  //       textAlign: TextAlign.center,
-                  //       style: TextStyle(
-                  //         fontSize: 40.0,
-                  //         fontWeight:
-                  //         FontWeight.bold,
-                  //       ),
-                  //     ),
-                  //   ),
-
+                  const SizedBox(height: 20.0),
                   SvgPicture.network('https://cdn.worldvectorlogo.com/logos/flutter-logo.svg'),
-
+                  const SizedBox(height: 20.0),
                   // Image.network('https://media.istockphoto.com/id/1323918152/photo/barumi-cityscape-with-seaside-boulevard-beach-and-skyscrapers.jpg?s=612x612&w=0&k=20&c=GRquQtB5v__2Xdqpmz4C1mjcBOO9-U_jBNxBhxmXCXs='),
                   CarouselSlider(
                     options: CarouselOptions(height: 400.0),
@@ -68,31 +52,27 @@ class MyApp extends StatelessWidget {
                         builder: (BuildContext context) {
                           return Container(
                             width: MediaQuery.of(context).size.width,
-                            margin: EdgeInsets.symmetric(horizontal: 5.0),
-                            decoration: BoxDecoration(
-                                color: Colors.amber
+                            margin: const EdgeInsets.symmetric(horizontal: 5.0),
+                            decoration: const BoxDecoration(
+                                color: Colors.amber,
                             ),
                             // child: Text('text $i', style: TextStyle(fontSize: 16.0),)
                             child: SvgPicture.network('https://upload.wikimedia.org/wikipedia/commons/4/44/Google-flutter-logo.svg'),
-
                           );
                         },
                       );
                     }).toList(),
                   ),
 
-                  SizedBox(height: 24.0),
-                  Text('text Widget',
+                  const SizedBox(height: 24.0),
+                  const Text('text Widget',
                     style: TextStyle(
                       fontFamily: 'RubikDistressed-Regular',
                       fontSize: 32,
                       // fontWeight: FontWeight.bold,
                       color: Colors.green,
                     ),
-                  ) ,
-                  SizedBox(height: 8.0),
-                  HintLabel('label 2')
-                  // HintLabel('custom label 1'),
+                  ) , 
                 ]),
           ),
         )
@@ -101,20 +81,4 @@ class MyApp extends StatelessWidget {
 }
 
 
-class HintLabel extends StatelessWidget {
-  final String text;
-  const HintLabel(this.text);
-  @override
-  Widget build(BuildContext context) {
-    return DecoratedBox(
-      decoration: BoxDecoration(color: Colors.amber[600]),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Text(text,
-            style: TextStyle(color: Colors.grey[700],
-              fontSize: 40,)
-        ),
-      ),
-    );
-  }
-}
+
